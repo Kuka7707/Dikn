2fe129bd0a717b8625b9c117dab0d6b82bf56855
kuka7707 <kuka7707@gmail.com>
2020-07-01 17:34:59 +0600

    extends React.Component

--- src/Pages/Users/Users.jsx
+++ src/Pages/Users/Users.jsx
@@ -1,39 +1,51 @@
 import React from 'react'
 import s from './Users.module.css'
 import { NavLink } from 'react-router-dom'
+import * as axios from 'axios'
+import userPhoto from '../../assets/imgs/default.jpg'
 
 
-const Users = (props) => {
-   return (
-      <div>
-         <h1>Пользователи</h1>
-         <hr />
-         <div className={s.Users}>
-            {props.users.map(u => <div className={s.user} key={u.id}>
-               {/*      {u.online ? :}            */}
-               {u.Online
-                  ? <NavLink to={'/users/'+u.id} className={s.online}>
-                     <img className={s.img} src={u.photoUrl} />
+
+class Users extends React.Component {
+   constructor(props) {
+      super(props);
+      axios.get("https://social-network.samuraijs.com/api/1.0/users")
+         .then(response => {
+            props.setUsers(response.data.items)
+         })
+   }
+   render() {
+      return (
+         <div>
+            <h1>Пользователи</h1>
+            <hr />
+            <div className={s.Users}>
+               {this.props.users.map(u => <div className={s.user} key={u.id}>
+                  {/*      {u.online ? :}            */}
+                  {u.Online
+                     ? <NavLink to={'/users/' + u.id} className={s.online}>
+                        <img className={s.img} src={u.photos.small != null ? u.photos.small : userPhoto} />
                      </NavLink>
-                  : <NavLink to={'/users/'+u.id} className={s.ofline}>
-                     <img className={s.img} src={u.photoUrl} />
+                     : <NavLink to={'/users/' + u.id} className={s.ofline}>
+                        <img className={s.img} src={u.photos.small != null ? u.photos.small : userPhoto} />
                      </NavLink>
-               }
-               <div className={s.info}>
-                  <NavLink to={'/users/'+u.id} className={s.name}>
-                     {u.Name}
-                  </NavLink>
-                  <div className={s.location}>
-                     {u.location.country} / {u.location.city}
+                  }
+                  <div className={s.info}>
+                     <NavLink to={'/users/' + u.id} className={s.name}>
+                        {u.name}
+                     </NavLink>
+                     {/* <div className={s.location}>
+                        {u.location.country} / {u.location.city}
+                     </div> */}
+                     <div className={s.status}>
+                        Статус: {u.Status}
+                     </div>
                   </div>
-                  <div className={s.status}>
-                     Статус: {u.Status}
-                  </div>
-               </div>
-            </div>)}
+               </div>)}
+            </div>
          </div>
-      </div>
-   )
+      )
+   }
 }
 
 export default Users
